2912416632ba87e76a23247d16e3cb1ba50c6460
Adr1an8 <67036270+Adr1an8@users.noreply.github.com>
2021-01-05 12:01:01 -0500

    Responsive Pages & Csp Bar Correction

--- views/meseros/EditarOrdenesMeseros.js
+++ views/meseros/EditarOrdenesMeseros.js
@@ -50,79 +50,6 @@ const EditarOrdenMeseros = (props) => {
 
     }
 
-    const handleChangeText = (value, name) => {
-        setState({ ...state, [name]: value });
-    };
-
-    // redirecciona a Progreso pedido
-    const progresoPedido = () => {
-        Alert.alert(
-            'Revisa tu pedido',
-            'Una vez que realizas tu pedido, no podrás cambiarlo',
-            [
-                {
-                    text: 'Confirmar',
-                    onPress: async () => {
-
-                        // crear un objeto
-                        const pedidoObj = {
-                            tiempoentrega: 0,
-                            completado: false,
-                            cancelado: false,
-                            total: Number(total),
-                            orden: pedido, // array
-                            creado: Date.now(),
-                            mesa: state.mesa
-                        }
-
-                        try {
-                            const pedido = await firebase.db.collection('ordenes').add(pedidoObj);
-                            pedidoRealizado(pedido.id);
-
-                            // redireccionar a progreso
-                            navigation.navigate("ProgresoPedido")
-                        } catch (error) {
-                            console.log(error);
-                        }
-
-
-                      
-                    }
-                }, 
-                { text: 'Revisar', style: 'cancel'}
-            ]
-        )
-    }
-
-    // Elimina un producto del arreglo de pedido
-    const confirmarEliminacion = id => {
-        Alert.alert(
-            '¿Deseas eliminar este artículo?',
-            'Una vez eliminado no se puede recuperar',
-            [
-                {
-                    text: 'Confirmar',
-                    onPress: async () => {
-                        
-                        try{
-                            const newObject = pedidos.filter(item => item.id !== id);
-                            const platoPedido = await firebase.db.collection('ordenes')
-                            .doc(idPlatoEditar)
-                            .update({
-                                orden: newObject
-                            })
-                            // redireccionar a progreso
-                            navigation.navigate("VerOrdenesMeseros")
-                            
-                        } catch (error) {
-                            console.log(error);
-                        }    
-                    }
-                }, 
-                { text: 'Cancelar', style: 'cancel'}
-            ]
-        )
-    }
     const EliminacionCompleta = () => {
         Alert.alert(
             'Desea cancelar la orden',
@@ -169,15 +96,6 @@ const EditarOrdenMeseros = (props) => {
                                     <Text>{nombre} </Text>
                                     <Text>Cantidad: {cantidad} </Text>
                                     <Text>Precio: $ {total} </Text>
-
-                                    <Button
-                                        onPress={ () => confirmarEliminacion(id) }
-                                        full
-                                        danger
-                                        style={{marginTop: 20}}
-                                    >
-                                        <Text style={[globalStyles.botonTexto, { color: '#FFF'}]}>Eliminar Plato</Text>
-                                    </Button>
                                 </Body>
                             </ListItem>
                         </List>
@@ -191,23 +109,15 @@ const EditarOrdenMeseros = (props) => {
                     >
                         <Text style={[globalStyles.botonTexto, { color: '#FFF'}]}>Eliminar</Text>
                     </Button>
-                <Button
-                    onPress={ () => navigation.navigate('Menu') }
-                    style={ {marginTop: 30}}
-                    full
-                    dark
-                >
-                    <Text style={[globalStyles.botonTexto, { color: '#FFF'}]}>Seguir Pidiendo</Text>
-                </Button>
             </Content>
             <Footer>
                 <FooterTab>
                     <Button
-                        // onPress={ () => progresoPedido()  }
+                        onPress={ () => navigation.navigate("VerOrdenesMeseros")  }
                          style={[globalStyles.boton ]}
                          full
                     >
-                        <Text style={globalStyles.botonTexto}>Ordenar Pedido</Text>
+                        <Text style={globalStyles.botonTexto}>Regresar</Text>
                     </Button>
                 </FooterTab>
             </Footer>
